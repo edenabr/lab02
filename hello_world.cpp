cd2bc666f09073ccef8579e5636c8d48f86dec4a
edenabr <edenabramova@gmail.com>
2026-05-25 15:46:48 +0000

    added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,12 @@
 
 int main()
 {
+    // Read user name from standard input
     std::string name;
     std::cin >> name;
+
+    // Print greeting message
     std::cout << "Hello world from @" << name << std::endl;
+
     return 0;
 }
